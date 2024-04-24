ab3c76ba4939e02e32774dc8f57415d134b6a65c
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-24 09:48:43 +0900

    Update chatbot_core.py

--- Project/travel_chat/chatbot_core.py
+++ Project/travel_chat/chatbot_core.py
@@ -254,7 +254,7 @@ def agent():
         tools=tool_amadeus,
         verbose=True,
         handle_parsing_errors="Check your output and make sure it conforms, use the Action/Action Input syntax",
-        max_iterations=5
+        max_iterations=10
     )
 
     tool_names = [tool.name for tool in tools]
