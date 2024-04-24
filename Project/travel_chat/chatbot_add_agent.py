1c849b743e2b3498b8d5d0a3dea414643fda732f
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-24 10:55:37 +0900

    Update chatbot_add_agent.py

--- Project/travel_chat/chatbot_add_agent.py
+++ Project/travel_chat/chatbot_add_agent.py
@@ -1,279 +1,198 @@
-import asyncio
-
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser, create_react_agent
-from langchain.prompts import StringPromptTemplate
-from langchain import OpenAI, SerpAPIWrapper, LLMChain
-from typing import List, Union
-from langchain.schema import AgentAction, AgentFinish, OutputParserException
-import re
-from langchain.agents.output_parsers import ReActJsonSingleInputOutputParser
+import json
+import requests
+from langchain.tools import BaseTool
+from langchain.agents import AgentType, initialize_agent, Tool
 from langchain.chat_models import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.tools import DuckDuckGoSearchRun
-from langchain import hub
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.tools.render import render_text_description_and_args
-from langchain_community.agent_toolkits.amadeus.toolkit import AmadeusToolkit
 
-import os
-import chainlit as cl
-import streamlit as st
-
-from chatbot_add_agent import all_in_1_agent
-
-# os.environ["OPENAI_API_KEY"] = "<openai-key>"
-# os.environ["GOOGLE_API_KEY"]
-os.environ["AMADEUS_CLIENT_ID"] = st.secrets["AMADEUS_CLIENT_ID"]
-os.environ["AMADEUS_CLIENT_SECRET"] = st.secrets["AMADEUS_CLIENT_SECRET"]
-toolkit = AmadeusToolkit()
-tool_amadeus = toolkit.get_tools()
-
-# template = """Answer the following questions as best you can, but speaking as passionate travel expert. You have access to the following tools:
-#
-# {tools}
-#
-# Use the following format:
-#
-# Question: the input question you must answer
-# Thought: you should always think about what to do
-# Action: the action to take, should be one of [{tool_names}]
-# Action Input: the input to the action
-# Observation: the result of the action
-# ... (this Thought/Action/Action Input/Observation can repeat N times)
-# Thought: I now know the final answer
-# Final Answer: a detailed day by day final answer to the original input question, You must answer in Korean
-#
-# Begin! Remember to answer as a passionate and informative travel expert when giving your final answer.
-#
-#
-# Question: {input}
-# {agent_scratchpad}"""
-
-template = """
-Answer the following questions as best you can, but speaking as passionate travel expert. You have access to the following tools:
-
-{tools}
-
-당신의 임무는 user가 원활하게 여행을 계획하고 마칠 수 있도록 돕는 것입니다.
-무조건 한국어로 답하십시오.
-다음 context와 history를 통해 user가 입력한 내용을 바탕으로 여행을 계획할 수 있도록 지원하세요.
-만약 user가 일정을 계획하기를 원한다면 user에게 몇 명과 함께 가는지, 누구와 가는지, 언제 가는지, 몇 박 몇 일 일정인지를 물어보고 이동거리를 고려한 일정을 세워주세요.
-user가 원한 기간 동안의 완벽한 일정이 수립되어야 합니다.
-해당 계획에는 항공편, 호텔 및 소요 예산이 포함됩니다.
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer,
-user가 원한 기간 동안의 완벽한 일정이 수립되어야 합니다,
-해당 계획에는 항공편, 호텔 및 소요 예산이 포함됩니다.
-Final Answer: the final answer to the original input question, You must answer in Korean, Do not omit it and print it out as is
+from typing import Optional, Type
+from pydantic import BaseModel, Field
 
 
-Begin! Remember to answer as a passionate and informative travel expert when giving your final answer.
-
-Previous conversation history:
-{history}
-
-Question: {input}
-{agent_scratchpad}
-"""
-
-template_with_history = """Answer the following questions as best you can, but speaking as passionate travel expert. You have access to the following tools:
-
-{tools}
-
-Use the following format:
-
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question, You must answer in Korean
-
-Begin! Remember to answer as a passionate and informative travel expert when giving your final answer.
-
-Previous conversation history:
-{history}
-
-Question: {input}
-{agent_scratchpad}"""
+class ProductInput(BaseModel):
+    """Get the product information."""
+    query_string: str = Field(...,
+                              description="The keyword for searching the product.")
+    max_price: float = Field(...,
+                             description="The maximum price of the product.")
+    min_price: float = Field(...,
+                             description="The minimum price of the product.")
+    special_price: bool = Field(...,
+                                description="Whether the product is on sale.")
+    point: bool = Field(...,
+                        description="Whether the product has LINE Point reward.")
+    line_badge: bool = Field(...,
+                             description="Whether the product is sold by 官方直營.")
+    line_pay: bool = Field(...,
+                           description="Whether the product can be paid by LINE Pay.")
+    color: list = Field(...,
+                        description="The color string of the product. e.g. [VIOLET,WHITE,BLACK]")
 
 
-# Set up a prompt template
-class CustomPromptTemplate(StringPromptTemplate):
-    # The template to use
-    template: str
-    # The list of tools available
-    tools: List[Tool]
+class ProductTool(BaseTool):
+    name = "search_product"
+    description = "Get the product information"
 
-    def format(self, **kwargs) -> str:
-        # Get the intermediate steps (AgentAction, Observation tuples)
-        # Format them in a particular way
-        intermediate_steps = kwargs.pop("intermediate_steps")
-        thoughts = ""
-        for action, observation in intermediate_steps:
-            thoughts += action.log
-            thoughts += f"\nObservation: {observation}\nThought: "
-        # Set the agent_scratchpad variable to that value
-        kwargs["agent_scratchpad"] = thoughts
-        # Create a tools variable from the list of tools provided
-        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
-        # Create a list of tool names for the tools provided
-        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
-        return self.template.format(**kwargs)
+    def _run(self, query_string: str, max_price: float, min_price: float, special_price: bool, point: bool,
+             line_badge: bool, line_pay: bool, color: list):
+        product_results = get_product(query_string,
+                                      max_price,
+                                      min_price,
+                                      special_price,
+                                      point,
+                                      line_badge,
+                                      line_pay,
+                                      color)
+        return product_results
 
+    def _arun(self, query_string: str, max_price: float, min_price: float, special_price: bool, point: bool,
+              line_badge: bool, line_pay: bool, color: list):
+        raise NotImplementedError("This tool does not support async")
 
-class CustomOutputParser(AgentOutputParser):
-
-    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
-        # Check if agent should finish
-        if "Final Answer:" in llm_output:
-            return AgentFinish(
-                # Return values is generally always a dictionary with a single `output` key
-                # It is not recommended to try anything else at the moment :)
-                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
-                log=llm_output,
-            )
-        # Parse out the action and action input
-        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
-        match = re.search(regex, llm_output, re.DOTALL)
-        if not match:
-            raise OutputParserException(f"Could not parse LLM output: `{llm_output}`")
-        action = match.group(1).strip()
-        action_input = match.group(2)
-        # Return the action and action input
-        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+    args_schema: Optional[Type[BaseModel]] = ProductInput
 
 
-def search_online(input_text):
-    search = DuckDuckGoSearchRun().run(f"site:tripadvisor.com things to do{input_text}")
-    return search
+# From LINE SHOPPING Product Search API.
+def get_product(query_string, max_price, min_price, special_price, point, line_badge, line_pay, color):
+    # def get_product(query_string):
+    url = "https://ec-gpt-plugin.vercel.app/api/get-product"
+
+    # Build the payload
+    payload = {
+        "queryString": query_string
+    }
+
+    # Only add the optional parameters to the payload if they are provided
+    if max_price > 10:
+        payload["maxPrice"] = max_price
+    if min_price > 10:
+        payload["minPrice"] = min_price
+    if special_price is not None:
+        payload["specialPrice"] = special_price
+    if point is not None:
+        payload["point"] = point
+    if line_badge is not None:
+        payload["lineBadge"] = line_badge
+    if line_pay is not None:
+        payload["linePay"] = line_pay
+    if color:
+        payload["color"] = color
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # Send POST request
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    if response.status_code == 200:
+        # Parse and return the JSON response body
+        # Get the JSON response
+        data = response.json()
+        # Limit the number of products to 10
+        if 'products' in data:
+            # Remove 'allMerchants' and 'merchant' from each product
+            for product in data['products']:
+                if 'allMerchants' in product:
+                    del product['allMerchants']
+                if 'merchant' in product:
+                    del product['merchant']
+            data['products'] = data['products'][:10]
+        return data
+    else:
+        # If the request failed, raise an exception
+        response.raise_for_status()
 
 
-def search_hotel(input_text):
-    search = DuckDuckGoSearchRun().run(f"site:booking.com {input_text}")
-    return search
+class TravelPOIInput(BaseModel):
+    """Get the keyword about travel information."""
+
+    keyword: str = Field(...,
+                         description="The city and state, e.g. San Francisco, CA")
 
 
-def search_flight(input_text):
-    search = DuckDuckGoSearchRun().run(f"site:skyscanner.com {input_text}")
-    return search
+class TravelPOITool(BaseTool):
+    name = "search_poi"
+    description = "Get the keyword about travel information"
+
+    def _run(self, keyword: str):
+        poi_results = get_pois(keyword)
+
+        return poi_results
+
+    def _arun(self, keyword: str):
+        raise NotImplementedError("This tool does not support async")
+
+    args_schema: Optional[Type[BaseModel]] = TravelPOIInput
 
 
-def search_general(input_text):
-    search = DuckDuckGoSearchRun().run(f"{input_text}")
-    return search
+def get_pois(keyword):
+    """
+    Query the get-poi API with the provided keyword.
+
+    Parameters:
+    keyword (str): The keyword for searching the position of interest.
+
+    Returns:
+    dict: The response from the API, should comply with getPoiResponse schema.
+    """
+    url = "https://nextjs-chatgpt-plugin-starter.vercel.app/api/get-poi"
+    headers = {'Content-Type': 'application/json'}
+
+    # The request data should comply with searchPoiRequest schema
+    data = {"keyword": keyword}
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.text}
 
 
-memory = ConversationBufferWindowMemory(k=2)
+class TravelExpInput(BaseModel):
+    """Get the keyword about travel experience."""
+
+    keyword: str = Field(...,
+                         description="The city and state, e.g. San Francisco, CA")
 
 
-def _handle_error(error) -> str:
-    return str(error)[:50]
+class TravelExpTool(BaseTool):
+    name = "search_experience"
+    description = "Get the keyword about travel experience"
+
+    def _run(self, keyword: str):
+        exp_results = get_experience(keyword)
+        return exp_results
+
+    def _arun(self, keyword: str):
+        raise NotImplementedError("This tool does not support async")
+
+    args_schema: Optional[Type[BaseModel]] = TravelExpInput
 
 
-@cl.on_chat_start
-def agent():
-    tools = [
+def get_experience(keyword):
+    api_url = "https://nextjs-chatgpt-plugin-starter.vercel.app/api/get-experience"
+    headers = {'Content-Type': 'application/json'}
 
-        Tool(
-            name="Search general",
-            func=search_general,
-            description="useful for when you need to answer general travel questions"
-        ),
-        Tool(
-            name="Search tripadvisor",
-            func=search_online,
-            description="useful for when you need to answer trip plan questions"
-        ),
-        Tool(
-            name="Search booking",
-            func=search_hotel,
-            description="useful for when you need to answer hotel questions"
-        ),
-        # Tool(
-        #     name="Search flight",
-        #     func=search_flight,
-        #     description="useful for when you need to answer flight questions"
-        # ),
-        Tool.from_function(
-            name="Search flight and airport",
-            func=lambda x: agent_executor2.invoke({"input": x}),
-            description="useful for when you need to answer flight questions and airport questions"
-        ),
-        Tool.from_function(
-            name="Search experience, poi and products",
-            func=lambda x: all_in_1_agent({"input": x}),
-            description="useful for when you need to answer travel experience questions or products questions or "
-                        "get the keyword about travel information"
-        )
-    ]
+    data = {
+        "keyword": keyword
+    }
 
-    prompt = CustomPromptTemplate(
-        template=template,
-        tools=tools,
-        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-        # This includes the `intermediate_steps` variable because that is needed
-        input_variables=["input", "intermediate_steps", "history"]
-    )
-    prompt_with_history = CustomPromptTemplate(
-        template=template,
-        tools=tools,
-        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
-        # This includes the `intermediate_steps` variable because that is needed
-        input_variables=["input", "intermediate_steps", "history"]
-    )
-    output_parser = CustomOutputParser()
-    # memory = ConversationBufferWindowMemory(k=2)
-    # llm = ChatOpenAI(temperature=0.7, model="gpt-3.5-turbo")
-    llm = ChatOpenAI(temperature=0.7, model="gpt-4-turbo")
-    # llm = ChatGoogleGenerativeAI(model="gemini-pro")
-    # LLM chain consisting of the LLM and a prompt
-    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
 
-    prompt_for_amadeus1 = hub.pull("hwchase17/react-json")
-    prompt_for_amadeus2 = """"
-    Here is instructions to use "tool":
-    departureDateTimeEarliest" and "departureDateTimeEarliest" MUST be the same date.
-    You can only find one day's worth at a time.
-    If you want to find flights for a multiple days, you have to use the "tool" N times for that period.
-    If you're trying to search for round-trip flights, call this function for the outbound flight first, and then call again for the return flight.
-    The currency must be converted to Korean Won before Final Answer."""
-    agent2 = create_react_agent(
-        llm,
-        tool_amadeus,
-        (prompt_for_amadeus1 + prompt_for_amadeus2),
-        tools_renderer=render_text_description_and_args,
-        output_parser=ReActJsonSingleInputOutputParser(),
-    )
-    agent_executor2 = AgentExecutor(
-        agent=agent2,
-        tools=tool_amadeus,
-        verbose=True,
-        handle_parsing_errors="Check your output and make sure it conforms, use the Action/Action Input syntax",
-        max_iterations=10
-    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
-    tool_names = [tool.name for tool in tools]
-    agent = LLMSingleActionAgent(
-        llm_chain=llm_chain,
-        output_parser=output_parser,
-        stop=["\nObservation:"],
-        allowed_tools=tool_names
-    )
-    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory,
-                                                        handle_parsing_errors="Check your output and make sure it conforms, use the Action/Action Input syntax",
-                                                        max_iterations=20)
 
-    return agent_executor
+def all_in_1_agent(input):
+    model = ChatOpenAI(model="gpt-4-turbo")
+    tools = [TravelPOITool(), TravelExpTool(), ProductTool()]
+    open_ai_agent = initialize_agent(tools,
+                                     model,
+                                     agent=AgentType.OPENAI_FUNCTIONS,
+                                     verbose=True)
+    tool_result = open_ai_agent.run(input)
+
+    return tool_result
