f7874ce6ee94aac3ff99cc027d083bb7cefe8440
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-26 15:18:48 +0900

    Update chatbot_core.py

--- Project/travel_chat/chatbot_core.py
+++ Project/travel_chat/chatbot_core.py
@@ -233,7 +233,11 @@ def agent():
             func=lambda x: sms_or_email().invoke({"input": x}),
             description="Send Email via Infobip. If you need to send email, use Send email",
         ),
-        SearchTool_hotel()
+        Tool.from_function(
+            name="hotel_search_tool",
+            func=SearchTool_hotel(),
+            description="useful when you need to search hotel"
+        ),
     ]
 
     prompt = CustomPromptTemplate(
