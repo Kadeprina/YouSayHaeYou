60d18acf6441f76fd40c63ae8d95849f9e0413ce
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-26 21:31:23 +0900

    Update hotel_core.py

--- Project/travel_chat/hotel_core.py
+++ Project/travel_chat/hotel_core.py
@@ -3,8 +3,8 @@ import os, sys
 
 @st.experimental_singleton
 def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+  os.system('sbase install chromedriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver /home/appuser/venv/bin/chromedriver')
 
 _ = installff()
 
