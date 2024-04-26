76855fc70fc07f0e31e4c501bf57e6bbc2e6a06c
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-26 22:26:40 +0900

    Update hotel_core.py

--- Project/travel_chat/hotel_core.py
+++ Project/travel_chat/hotel_core.py
@@ -19,11 +19,28 @@ from langchain.tools import BaseTool, StructuredTool, tool
 # service = Service(ChromeDriverManager(driver_version=chrome_version).install())
 # driver = webdriver.Chrome(service=service)
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
+
+    @st.cache_resource
+    def get_driver():
+        return webdriver.Chrome(
+            service=Service(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=options,
+        )
+
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+
+    driver = get_driver()
+    driver.get("http://example.com")
 
 
 class schema_hotel(BaseModel):
