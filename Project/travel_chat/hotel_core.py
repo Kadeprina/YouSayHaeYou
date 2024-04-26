cee561cb8961152674ccbda42886ba7ae1ad30a8
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-26 21:19:04 +0900

    Update hotel_core.py

--- Project/travel_chat/hotel_core.py
+++ Project/travel_chat/hotel_core.py
@@ -1,3 +1,13 @@
+import streamlit as st
+import os, sys
+
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from urllib.parse import urlencode
