96a6a428514805a747a5c24c3bb30d83cd92d8b2
Kadeprina <166676171+Kadeprina@users.noreply.github.com>
2024-04-26 21:34:51 +0900

    Update hotel_core.py

--- Project/travel_chat/hotel_core.py
+++ Project/travel_chat/hotel_core.py
@@ -1,12 +1,26 @@
 import streamlit as st
-import os, sys
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+    from webdriver_manager.core.os_manager import ChromeType
 
-@st.experimental_singleton
-def installff():
-  os.system('sbase install chromedriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver /home/appuser/venv/bin/chromedriver')
+    @st.cache_resource
+    def get_driver():
+        return webdriver.Chrome(
+            service=Service(
+                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+            ),
+            options=options,
+        )
 
-_ = installff()
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless")
+
+    driver = get_driver()
+    driver.get("http://example.com")
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
